8589fa0ebcdd9c09ee07b1d7649b0e3a2b83383a
VusalNerimanov <vusalnerimanov58@gmail.com>
2024-03-17 14:29:32 +0400

    Add project files.

---
+++ calculator.3/Program.cs
@@ -0,0 +1,51 @@
+﻿
+Console.Write("enter first number:");
+double num1 =double.Parse(Console.ReadLine());
+
+Console.Write("enter sybol:");
+char symbol = char.Parse(Console.ReadLine());
+
+Console.Write("enter second number:");
+double num2 = double.Parse(Console.ReadLine());
+
+if (symbol == '+')
+{
+    Console.WriteLine(num1 + num2);
+}
+
+if (symbol == '-')
+{
+    Console.WriteLine(num1 - num2); 
+}
+
+if (symbol == '*')
+{
+    Console.WriteLine(num1 * num2); 
+}
+
+if (symbol == '/')
+{  
+    if (num2 == 0)
+
+        Console.WriteLine("eded 0 A bolune bilmez");
+
+    Console.WriteLine(num1 / num2);      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
